777cb6e89d411e60d99a9d10d555ed96cde3cffb
absolutetsonga <abstsonga@gmail.com>
2024-01-06 05:23:37 +0600

    feat: images.remotePatterns added, which gave access to the player images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.a.transfermarkt.technology",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "tmssl.akamaized.net",
+        pathname: "**",
+      },
+    ],
+  },
+
   swcPlugins: [["@swc-jotai/react-refresh", {}]],
 };
 
