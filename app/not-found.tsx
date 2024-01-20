e13b948a28d829037d429796d2617e876797649f
absolutetsonga <abstsonga@gmail.com>
2024-01-20 16:13:34 +0600

    feat: not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,7 @@
-import { PageContainer } from "@/src/07_shared/ui";
+import NotFoundPage from "@/src/03_pages/not-found";
 
-export default function NotFound() {
-  return (
-    <PageContainer>
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-          Page not found
-        </h1>
-        <p className="mt-6 text-base leading-7">
-          Sorry, we could not find the page you are looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </a>
-        </div>
-      </div>
-    </PageContainer>
-  );
-}
+const Page = () => {
+  return <NotFoundPage />;
+};
+
+export default Page;
