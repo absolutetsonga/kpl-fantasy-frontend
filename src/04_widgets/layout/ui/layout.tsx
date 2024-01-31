736bd6a31860c412a92cd5cf1d9a8b8c15445aad
absolutetsonga <abstsonga@gmail.com>
2024-01-31 15:28:32 +0600

    feat: admin pages required auth user

--- src/04_widgets/layout/ui/layout.tsx
+++ src/04_widgets/layout/ui/layout.tsx
@@ -1,6 +1,6 @@
 import { RequireAuth } from "@/src/05_features/auth-user/ui";
 import React from "react";
 
-export const DraftLayout = ({ children }: { children: React.ReactNode }) => {
+export const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return <RequireAuth>{children}</RequireAuth>;
 };
