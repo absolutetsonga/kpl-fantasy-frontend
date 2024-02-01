29c5c6eb212ced7195b0e30361b731739cb96236
absolutetsonga <abstsonga@gmail.com>
2024-02-01 22:48:07 +0600

    modified: admin false in admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import { PageLayout } from "@/src/04_widgets/layout/ui/layout";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <PageLayout admin={true}>{children}</PageLayout>;
+  return <PageLayout admin={false}>{children}</PageLayout>;
 };
 
 export default Layout;
